64afcac250266c82c4b0ba60de056ea157f7720f
PJDEEPESH <167789969+PJDEEPESH@users.noreply.github.com>
2024-06-09 23:48:53 +0530

    TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,24 @@
 import React from "react";
 
-const TodoList = ({ todos, handleDelete, handleEdit }) => {
+const TodoList = ({ todos, handleDelete, handleEdit, handleMarkComplete }) => {
   return (
     <ul className="allTodos">
       {todos.map((t) => (
-        <li className="singleTodo">
-          <span className="todoText" key={t.id}>
+        <li className="singleTodo" key={t.id}>
+          <span
+            className="todoText"
+            style={{
+              textDecoration: t.completed ? "line-through" : "none",
+              color: t.completed ? "green" : "black",
+            }}
+          >
             {t.todo}
           </span>
           <button onClick={() => handleEdit(t.id)}>Edit</button>
           <button onClick={() => handleDelete(t.id)}>Delete</button>
+          <button onClick={() => handleMarkComplete(t.id)}>
+            {t.completed ? "Unmark" : "Complete"}
+          </button>
         </li>
       ))}
     </ul>
