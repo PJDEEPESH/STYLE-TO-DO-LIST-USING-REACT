0ded60f90cfe5ddd4832ae21f3680c05d2fe450f
PJDEEPESH <deepeshp.j.2004@gmail.com>
2024-06-09 17:02:24 +0530

    first commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TodoForm = ({ handleSubmit, todo, editId, setTodo }) => {
+  return (
+    <form className="todoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter your task"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="submit"> {editId ? "Edit" : "Go"}</button>
+    </form>
+  );
+};
+
+export default TodoForm;
